28fdc6363fdc9eeadb99951b8e790e6b819ab76d
Byron Cheng <byroncheng@gmail.com>
2014-03-15 19:13:38 -0500

    Added geolocation code

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
+
     var apiKey = "047a228973952d4d1076f8d0f2968744";
     var latitude = "37.424123";
     var longitude = "-122.103233";
+
+    $('#GetLoc').click(function(){
+        if (navigator.geolocation){
+          navigator.geolocation.getCurrentPosition(showPosition);
+        }
+        else {
+            $('#Location').innerHTML="Geolocation is not supported by this browser.";
+        }
+    });
+
+    function showPosition(position){
+      $('#Location').innerHTML="Latitude: " + position.coords.latitude + 
+      "<br>Longitude: " + position.coords.longitude;    
+    };
+
 
     var weatherAPI = "https://api.forecast.io/forecast/";
     $('button').click(function() {
