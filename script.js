aed5ddd044f89fb81e34b1d5778a3c12b5bcc967
unknown <03349193@PEPWUL72285.corp.pep.pvt>
2014-03-16 13:35:29 -0500

    fixed geolocation

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
-    var apiKey = "047a228973952d4d1076f8d0f2968744";
-    var latitude = "37.424123";
-    var longitude = "-122.103233";
+    /*var latitude = "37.424123";
+    var longitude = "-122.103233";*/
+
+    var latitude;
+    var longitude;
 
     $('#GetLoc').click(function(){
         if (navigator.geolocation){
@@ -14,15 +16,19 @@ $(document).ready(function() {
     });
 
     function showPosition(position){
-      $('#Location').innerHTML="Latitude: " + position.coords.latitude + 
-      "<br>Longitude: " + position.coords.longitude;    
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      $('#Location').html("Latitude: " + latitude + 
+      "<br>Longitude: " + longitude);    
     };
 
 
     var weatherAPI = "https://api.forecast.io/forecast/";
-    $('button').click(function() {
+    var apiKey = "047a228973952d4d1076f8d0f2968744";
+
+    $('#GetWeather').click(function() {
         $.getJSON(weatherAPI + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
-          $('#data').append('The temperature is : ' + data.currently.temperature);
+          $('#Weather').html('The temperature is : ' + data.currently.temperature);
 
         });
     });
