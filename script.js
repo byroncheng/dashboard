d1c366539d33451c70aa26c6c6ab134635259baf
Byron Cheng <byroncheng@gmail.com>
2014-03-17 22:53:40 -0500

    split out getting weather data and displaying data

--- script.js
+++ script.js
@@ -5,32 +5,48 @@ $(document).ready(function() {
 
     var latitude;
     var longitude;
-
-    $('#GetLoc').click(function(){
-        if (navigator.geolocation){
-          navigator.geolocation.getCurrentPosition(showPosition);
-        }
-        else {
-            $('#Location').innerHTML="Geolocation is not supported by this browser.";
-        }
-    });
-
-    function showPosition(position){
-      latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
-      $('#Location').html("Latitude: " + latitude + 
-      "<br>Longitude: " + longitude);    
-    };
-
+    var weatherData;
 
     var weatherAPI = "https://api.forecast.io/forecast/";
     var apiKey = "047a228973952d4d1076f8d0f2968744";
 
-    $('#GetWeather').click(function() {
-        $.getJSON(weatherAPI + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
-          $('#Weather').html('The temperature is : ' + data.currently.temperature +'<br>The possibility of precipitation is ' + data.currently.precipProbability*100 +'%');
-
-        });
+    $('#GetData').click(function(){
+      if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(getPosition);
+      }
+      else {
+        $('#Location').innerHTML="Geolocation is not supported by this browser.";
+      }
     });
 
-});
+    function getPosition(position){
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      $('#Location').html("Latitude: " + latitude + "<br>Longitude: " + longitude);
+      getWeather();
+    };
+
+    function getWeather(){
+      $.getJSON(weatherAPI + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
+        weatherData=data;
+      }
+    )}
+
+
+    $('#ShowData').click(function() {
+      $('#Weather').html(
+        'The temperature is : ' + weatherData.currently.temperature
+        +'<br>The possibility of precipitation is '+ weatherData.currently.precipProbability*100
+        +'%<br>The current conditions are ' + weatherData.currently.icon
+        + getPrecipType()
+        );
+    });
+
+    function getPrecipType(){
+      if (weatherData.currently.precipIntensity != 0) {return '<br>It is currently ' + weatherData.currently.precipType;}
+      else {return "";}
+    }
+
+
+
+  });
